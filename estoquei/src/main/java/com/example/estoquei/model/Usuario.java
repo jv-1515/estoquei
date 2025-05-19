7b4e1f23fb62d9b674240539c5d5666e6c36b387
Adriano Fraga <adriano.rfraga05@gmail.com>
2025-05-18 22:38:01 -0300

    Criação das classes base

--- estoquei/src/main/java/com/example/estoquei/model/Usuario.java
+++ estoquei/src/main/java/com/example/estoquei/model/Usuario.java
@@ -1,5 +1,36 @@
 package com.example.estoquei.model;
 
 public class Usuario {
+    private String id;
+    private String nome;
+    private TipoUsuario tipo;
 
+    public Usuario(){
+
+    }
+
+    public Usuario(String nome, TipoUsuario tipo){
+        this.nome=nome;
+        this.tipo=tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
 }
