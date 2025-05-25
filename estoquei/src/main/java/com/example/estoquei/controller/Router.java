28849153fab538030f2183abb9d59091ff28a2f1
João Victor Alves <jvsilva@bdsdatasolution.com.br>
2025-05-25 01:53:30 -0300

    Correção das rotas

--- estoquei/src/main/java/com/example/estoquei/controller/Router.java
+++ estoquei/src/main/java/com/example/estoquei/controller/Router.java
@@ -4,17 +4,73 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/")
 public class Router {
 
     @GetMapping
-    public String index() {
-        return "index";
+    public String login() {
+        return "login";
     }
 
-    @GetMapping("/cadastro")
+    @GetMapping("/cadastrar-produto")
     public String cadastro() {
-        return "cadastro_produto";
+        return "cadastrar_produto";
     }
+
+    @GetMapping("/inicio")
+    public String inicio() {
+        return "inicio";
+    }
+    
+
+    // @GetMapping("/estoque")
+    // public String estoque() {
+    //     return "estoque";
+    // }
+
+    // @GetMapping("/reabastecer-produto")
+    // public String reabastecerProduto() {
+    //     return "reabastecer_produto";
+    // }
+
+    // @GetMapping("/visualizar-produto")
+    // public String visualizarProduto() {
+    //     return "visualizar_produto";
+    // }
+    
+    // @GetMapping("/editar-produto")
+    // public String editarProduto() {
+    //     return "editar_produto";
+    // }
+
+
+    // @GetMapping("/visualizar-funcionario")
+    // public String visualizarFuncionario() {
+    //     return "visualizar_funcionario";
+    // }
+    // @GetMapping("/cadastrar-funcionario")
+    // public String cadastrarFuncionario")() {
+    //     return "cadastrar_funcionario");
+    // }
+
+    // @GetMapping("/editar-funcionario")
+    // public String editarFuncionario() {
+    //     return "editar_funcionario";
+    // }
+
+    // @GetMapping("/visualizar-fornecedor")
+    // public String visualizarFornecedor() {
+    //     return "visualizar_fornecedor";
+
+    // @GetMapping("/cadastrar-fornecedor")
+    // public String cadastrarFornecedor() {
+    //     return "cadastrar_fornecedor";
+    // }
+
+    // @GetMapping("/editar-fornecedor")
+    // public String editarFornecedor() {
+    //     return "editar_fornecedor";
+    // }
 }
