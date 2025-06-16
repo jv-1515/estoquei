50da2c4ecb1f84766d55a8554022d724912e6fac
João Victor <joao.silva@fatec.sp.gov.br>
2025-06-16 16:58:53 -0300

    Criação das páginas de erro e andamento

---
+++ estoquei/target/classes/static/js/andamento.js
@@ -0,0 +1,21 @@
+    let progress = 0;
+    const progressBar = document.getElementById('progress-bar');
+    const progressContainer = progressBar.parentElement;
+    const backButton = document.getElementsByClassName('btn-voltar')[0];
+
+    backButton.style.display = 'none';
+
+    function animateProgress() {
+        if (progress <= 80) {
+            progressBar.style.width = progress + '%';
+            progressBar.textContent = progress + '%';
+            progress++;
+            setTimeout(animateProgress, 30);
+        } else if (progress > 80) {
+            progressBar.style.display = 'none';
+            backButton.style.display = 'block'; 
+            document.getElementsByClassName('progress-bar-bg')[0].style.backgroundColor = 'white';
+            document.getElementsByClassName('progress-bar-bg')[0].style.margin = '0';
+        }
+    }
+    animateProgress();
