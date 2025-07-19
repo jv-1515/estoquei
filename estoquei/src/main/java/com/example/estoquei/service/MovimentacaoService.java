2189aad1e4cdae8bb37eebe4259d11a286960680
João Victor <joao.silva@fatec.sp.gov.br>
2025-07-19 04:03:30 -0300

    Unificação da tabela e melhorias de lógica

--- estoquei/src/main/java/com/example/estoquei/service/MovimentacaoService.java
+++ estoquei/src/main/java/com/example/estoquei/service/MovimentacaoService.java
@@ -1,12 +1,13 @@
 package com.example.estoquei.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.estoquei.model.EntradaProduto;
 import com.example.estoquei.model.MovimentacaoProduto;
 import com.example.estoquei.model.Produto;
-import com.example.estoquei.model.SaidaProduto;
 import com.example.estoquei.repository.MovimentacaoProdutoRepository;
 
 @Service
@@ -15,38 +16,40 @@ public class MovimentacaoService {
     @Autowired
     private MovimentacaoProdutoRepository movimentacaoRepo;
     
-    public void registrarEntrada(EntradaProduto entrada, Produto produto) {
+    // MÉTODO UNIFICADO PARA ENTRADAS
+    public void registrarEntrada(String codigoCompra, int quantidade, BigDecimal valorCompra, String fornecedor, Produto produto) {
         MovimentacaoProduto movimentacao = new MovimentacaoProduto(
-            entrada.getDataEntrada(),
+            LocalDate.now(),
             produto.getCodigo(),
             produto.getNome(),
             produto.getCategoria(),
             produto.getTamanho(),
             produto.getGenero(),
             "ENTRADA",
-            entrada.getCodigoCompra(),
-            entrada.getQuantidade(),
+            codigoCompra,
+            quantidade,
             produto.getQuantidade(),
-            entrada.getValorCompra(),
-            entrada.getFornecedor()
+            valorCompra,
+            fornecedor
         );
         movimentacaoRepo.save(movimentacao);
     }
     
-    public void registrarSaida(SaidaProduto saida, Produto produto) {
+    // MÉTODO UNIFICADO PARA SAÍDAS
+    public void registrarSaida(String codigoVenda, int quantidade, BigDecimal valorVenda, String comprador, Produto produto) {
         MovimentacaoProduto movimentacao = new MovimentacaoProduto(
-            saida.getDataSaida(),
+            LocalDate.now(),
             produto.getCodigo(),
             produto.getNome(),
             produto.getCategoria(),
             produto.getTamanho(),
             produto.getGenero(),
             "SAIDA",
-            saida.getCodigoVenda(),
-            saida.getQuantidade(),
+            codigoVenda,
+            quantidade,
             produto.getQuantidade(),
-            saida.getValorVenda(),
-            saida.getComprador()
+            valorVenda,
+            comprador
         );
         movimentacaoRepo.save(movimentacao);
     }
