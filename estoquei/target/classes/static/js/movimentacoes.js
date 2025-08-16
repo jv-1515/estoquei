5fc6a795431dfa46dd80222d146c1603987a0ea9
João Victor <joao.silva@fatec.sp.gov.br>
2025-08-16 20:50:17 -0300

    Ajuste dos placeholders e ajustes gerais

--- estoquei/target/classes/static/js/movimentacoes.js
+++ estoquei/target/classes/static/js/movimentacoes.js
@@ -279,7 +279,11 @@ function aplicarFiltroValorFaixa() {
     // Formata para o input principal
     let minStr = minNum.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});
     let maxStr = maxNum.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-    valorInput.value = `R$ ${minStr} - R$ ${maxStr}`;
+    if (minNum === maxNum) {
+        valorInput.value = `R$ ${minStr}`;
+    } else {
+        valorInput.value = `R$ ${minStr} - R$ ${maxStr}`;
+    }
     if (valorInput.value === "R$ 0,00 - R$ 999.999,99") {
         valorInput.value = "Todos";
         ativo = false;
@@ -1213,7 +1217,12 @@ function aplicarFiltroQtdFaixa() {
     if (min > max) [min, max] = [max, min];
     qtdMin.value = min;
     qtdMax.value = max;
-    qtdInput.value = `${min} - ${max}`;
+
+    if (min === max) {
+        qtdInput.value = `${min}`;
+    } else {
+        qtdInput.value = `${min} - ${max}`;
+    }
     if (qtdInput.value === "0 - 999") {
         qtdInput.value = "Todas";
         ativo = false;
