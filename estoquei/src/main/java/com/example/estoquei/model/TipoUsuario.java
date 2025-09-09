6b53654c114f4bb5c1506f11ec48625ce6fe6311
João Victor <joao.silva@fatec.sp.gov.br>
2025-09-08 23:05:29 -0300

    Ajuste para reverter tabela

--- estoquei/src/main/java/com/example/estoquei/model/TipoUsuario.java
+++ estoquei/src/main/java/com/example/estoquei/model/TipoUsuario.java
@@ -1,15 +1,15 @@
-package com.example.estoquei.model;
+// package com.example.estoquei.model;
 
-public enum TipoUsuario {
-    FUNCIONARIO(0), GERENTE(1), ADMIN(2);
+// public enum TipoUsuario {
+//     FUNCIONARIO(0), GERENTE(1), ADMIN(2);
 
-    private final int value;
+//     private final int value;
 
-    TipoUsuario(int value) {
-        this.value = value;
-    }
+//     TipoUsuario(int value) {
+//         this.value = value;
+//     }
 
-    public int getValue() {
-        return value;
-    }
-}
+//     public int getValue() {
+//         return value;
+//     }
+// }
